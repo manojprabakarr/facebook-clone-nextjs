2c77716f71961272175fd46df2ac90b5ff75c00b
manojprabakarr <manojprabakarrr52gmail.com>
2021-07-02 23:14:45 +0530

    final

--- components/Post.js
+++ components/Post.js
@@ -3,7 +3,7 @@ import CommentIcon from '@material-ui/icons/Comment';
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 import ShareIcon from '@material-ui/icons/Share';
 
-function Post({message,time,name,postdata,profile}) {
+function Post({message,timestamp,name,postdata,profile}) {
     return (
         
         <div className="flex flex-col justify-center rounded-2xl bg-white  h-auto mb-8 mr-6 ">
@@ -13,9 +13,9 @@ function Post({message,time,name,postdata,profile}) {
         <Avatar src={profile}/>
         <div className="flex-col ml-2 ">
         <h1 className="text-xl">{name}</h1>
-        <h3 className="text-gray-500 text-sm mb-6">12:00am wednesday</h3></div></div>
+        <h3 className="text-gray-500 text-sm mb-6"> {new Date(timestamp?.toDate()).toLocaleString()}</h3></div></div>
           <h4 className="text-xl font-mono mb-2 ml-4">{message}</h4>
-         <img className="object-fill rounded-xl" src={postdata} />
+         <img className="object-fill rounded-3xl" src={postdata} />
         
         <div className="flex mt-4 justify-evenly text-gray-500">
         <FavoriteBorderIcon className="hover:text-red-200" />
